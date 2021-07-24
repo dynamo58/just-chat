1b53e8fd4fa040b2585ff0a719578efe4ac11668
dynamo58 <sailorfirex@seznam.cz>
2021-07-24 03:36:17 +0200

    test-build3

--- server/index.js
+++ server/index.js
@@ -18,6 +18,9 @@ http.listen(PORT, () => {
 const connectionString = process.env.DATABASE_URL
 const client = new Client({
   connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 client.connect();
